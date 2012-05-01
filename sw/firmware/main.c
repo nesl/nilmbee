0a63083bb9cbff209a9892d5ffcd489cff81f002
wtj <wtj@5ca23f20-e367-4d64-8eff-f2ce32bc6055>
2012-05-01 04:53:26 +0000

    support multiple states.

--- sw/firmware/main.c
+++ sw/firmware/main.c
@@ -11,6 +11,9 @@ uint8_t delay_count = 0;
 uint8_t rand_counter;
 uint8_t next_delay;
 
+#define DEBOUNCE_1 7
+#define DEBOUNCE_2 15
+
 void inline adc_init(void)
 {
     //PRR = 0;
@@ -41,9 +44,9 @@ int main(void)
 {
     //uint8_t last_is_zero = 1;
 
-    uint8_t i, dirty;
-    uint8_t adcv, adcmax, adcmin;
-    uint8_t score = 0, score_shifter = 0;
+    uint8_t i, dirty, active = 0;
+    uint8_t adcv, adcmax, adcmin, lastadc = 0;
+    uint8_t score = 16, score2 = 16;
 
     pin_init();
     adc_init();
@@ -61,51 +64,66 @@ int main(void)
             if (adcv > adcmax) adcmax = adcv;
             if (adcv < adcmin) adcmin = adcv;
         }
-        rand_counter ++ ;
-        
-        //is_active = (adcmax - adcmin > 3);
-        //score = score + is_active - (score_shifter >= 0x80);
-        //score_shifter = (score_shifter<<1) | is_active;
-        adcmin += 10;
-        if (!(msg.byte2 & 1)) adcmin+=2;
-        asm (
-            "cp   %2, %3" "\n\t"
-            "brcc L_sc1 " "\n\t"
-            "inc  %0" "\n\t"
-"L_sc1:   " "rol  %1" "\n\t"
-            "brcc L_sc2 " "\n\t"
-            "dec  %0" "\n\t"
-"L_sc2:   " "" "\n\t"
-            "" "\n\t"
-            : "=r" (score), "=r" (score_shifter)
-            : "r" (adcmin), "r" (adcmax), "r" (score), "r" (score_shifter)
-        );
+        rand_counter += adcv ;
         
         dirty = 0;
-        if ((score <= 2) && (msg.byte2 & 1)) {
+        adcmax = adcmax - adcmin;
+        adcmin = 12;
+        //i = (msg.byte2 & 1);
+        if (active) adcmin-=2;
+        
+        score +=1;
+        if (adcmax<adcmin) score -=2;
+        if (score == 16-DEBOUNCE_1-1) score = 16-DEBOUNCE_1;
+        if (score == 16+DEBOUNCE_1+1) score = 16+DEBOUNCE_1;
+        
+        if (adcmax > lastadc && adcmax-lastadc > 10) {
+            score2 += 1;
+        } else if (adcmax <= lastadc && lastadc-adcmax > 10) {
+            score2 -= 1;
+        } else {
+            score2 = 16;
+        }
+
+        if (active && score <= 16-DEBOUNCE_1) {
             dirty = 1;
+            msg.byte1 &= ~0x80;
             // emu: msg.is_active = 0;
             // emu: msg.delay_pos = 0;
-            msg.byte2 = (msg.byte2 & 0xF0);
-        }
-        if ((score >= 6) && !(msg.byte2 & 1)) {
+            active = 0;
+            msg.byte2 &= 0xFE;
+        } else if (!active && score >= 16+DEBOUNCE_1) {
             dirty = 1;
+            msg.byte1 &= ~0x80;
             // emu: msg.is_active = 1;
             // emu: msg.delay_pos = 0;
-            msg.byte2 = (msg.byte2 & 0xF0) | 0x1;
+            active = 1;
+            msg.byte2 |= 0x1;
+        } else if (score2 == 16-DEBOUNCE_2-1) {
+            msg.byte1 |= 0x80;
+            msg.byte2 &= 0xFE;
+            dirty = 1;
+        } else if (score2 == 16+DEBOUNCE_2+1) {
+            msg.byte1 |= 0x80;
+            msg.byte2 |= 0x1;
+            dirty = 1;
         }
         if (dirty) {
+            lastadc = adcmax;
+            score2 = 16;
             // emu: msg.delay_grp = 0;
             msg.byte1 &= ~0x3;
+            msg.byte2 = (msg.byte2 & 0xF1);
             
             cli();
             tx_word(msg);
             msg.byte2 += 0x10;
             sei();
             
-            next_delay = 0xC0 | ((rand_counter & 0x0E) << 2);
+            i = (rand_counter & 0x0E);
+            next_delay = 0xC0 | (i << 2);
             msg.byte1 |= 0x3;
-            msg.byte2 |= (rand_counter & 0x0E);
+            msg.byte2 |= i;
             delay_count = 254;
         }
         //tx_word(adcmax - adcmin);
@@ -114,15 +132,17 @@ int main(void)
 
 ISR(PCINT0_vect)
 {
+    uint8_t i;
     if ((PINB & (1<<1)) && delay_count) {
         delay_count -= 2;
         if (delay_count == next_delay) {
             tx_word(msg);
             msg.byte2 += 0x10;
             next_delay = (delay_count & 0xC0) - 0x40; 
-            next_delay |= ((rand_counter & 0x0E) << 2);
+            i = (rand_counter & 0x0E);
+            next_delay |= (i << 2);
             msg.byte1 = (msg.byte1 & 0xFC) | (next_delay >> 6);
-            msg.byte2 = (msg.byte2 & 0xF1) | (rand_counter & 0x0E);
+            msg.byte2 = (msg.byte2 & 0xF1) | i;
             if (!next_delay) {
                 next_delay = 0x08;
                 msg.byte2 += 0x02;
