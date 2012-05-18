bdd7ccf7d5322d668ee351944fb8797dbc6bb404
wtj <wtj@5ca23f20-e367-4d64-8eff-f2ce32bc6055>
2012-05-18 20:19:22 +0000

    update status page with color add query page

---
+++ sw/pc/wattsup/web/query.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "db.php";
+
+db_conn();
+
+$t1 = (int)$_REQUEST['t1'];
+$t2 = (int)$_REQUEST['t2'];
+$id = (int)$_REQUEST['id'];
+
+if (!$t2) $t2 = time();
+
+$sql = "select unix_timestamp(timestamp) as t, data as d from log where unix_timestamp(timestamp) > $t1 and unix_timestamp(timestamp) < $t2 and wattsup_id = $id";
+$res = mysql_query($sql);
+
+header("Content-Type: text/plain");
+
+while ($data = mysql_fetch_assoc($res)) {
+    $d = $data['d'];
+    $d = explode(',',$d);
+    $d = array(((float)$d[3])/10, ((float)$d[4]/10), ((float)$d[5]/1000), ((float)$d[6]/10), ((float)$d[16]/100), ((float)$d[19]/10));
+    echo "${data['t']}\t${d[0]}\t${d[1]}\t${d[2]}\t${d[3]}\t${d[4]}\t${d[5]}\n";
+}
+
+
--- sw/pc/wattsup/web/status.php
+++ sw/pc/wattsup/web/status.php
@@ -4,14 +4,18 @@ require_once "db.php";
 
 db_conn();
 
-$sql = "select count(*) as c, wattsup_id from log where timestampdiff(MINUTE, timestamp, now()) <= 10 group by wattsup_id";
+$sql = "select count(*) as c, wattsup_id from log where timestamp>=timestampadd(MINUTE, -10, now()) group by wattsup_id";
 $res = mysql_query($sql);
 
 
 echo "<h3>Recent (10min) log counts</h3>";
 while ($data = mysql_fetch_assoc($res)) {
     $f = round($data['c'] / 600, 2);
-    echo "${data['wattsup_id']} - ${data['c']} - ($f / s) <br/>";
+    if ($f < 0.46) 
+        $color = "#800";
+    else
+        $color = "#080";
+    echo "<span style=\"color:$color\">${data['wattsup_id']} - ${data['c']} - ($f / s) </span><br/>";
 }
 
 
